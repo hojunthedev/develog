69c5eadce2fa5a35aa34c497aebfdedd0f734e3e
hojunthedev <ghwns5338@gmail.com>
2021-02-13 23:07:47 +0900

    개발환경설정_1
    jdk, path, sts install, mysql, intellij key map, font setting

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,11 @@
+// enc UTF-8
+// 키맵 = 인텔리J = 실행은 쉬프트 f10
+public class HelloWorld {
+	//쿠키런 폰트는 좋네요!
+	public static void main(String[] args) {
+		System.out.println("안뇽");
+		System.out.println("안뇽");
+		System.out.println("안뇽");
+		System.out.println("안뇽");
+	}
+}
