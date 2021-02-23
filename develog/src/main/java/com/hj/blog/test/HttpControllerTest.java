87f96833e3ccc2a49c223336a0f4b4dd61b34495
hojunthedev <ghwns5338@gmail.com>
2021-02-23 16:40:08 +0900

    롬복 세팅,테스트

--- develog/src/main/java/com/hj/blog/test/HttpControllerTest.java
+++ develog/src/main/java/com/hj/blog/test/HttpControllerTest.java
@@ -13,6 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//빌더 패턴_1.순서를 지키지 않아도 된다/2.초기화 하고싶은 멤버만 초기화 가능
+		Member m = Member.builder().username("ssar").password("1234").email("ghwnsGMAIL").build();
+		System.out.println(TAG + "getter : " + m.getUsername());
+		m.setUsername("hojun");
+		System.out.println(TAG + "setter : " + m.getUsername());
+		return "lombok test 완료";
+	}
+	
 	// 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
--- develog/src/main/java/com/hj/blog/test/Member.java
+++ develog/src/main/java/com/hj/blog/test/Member.java
@@ -1,43 +1,24 @@
 package com.hj.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
 	
-	
+	@Builder	//롬복의 기능. 생성자 오버로딩 필요없이, 초기화하고싶은 값만 초기화 할수있게 해준다
 	public Member(int id, String username, String password, String email) {
+		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
 }
