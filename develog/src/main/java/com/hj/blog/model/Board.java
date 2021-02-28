314104339c1011b2a8fdecddb2d92e54887a6874
hojunthedev <ghwns5338@gmail.com>
2021-02-28 21:35:04 +0900

    외래키의 주인, 패치전략
    ManyToOne의 기본 패치전략은 EAGER(바로 들고오기)
    OneToMany의 기본 패치전략은 LAZY(필요하면 들고올게)

--- develog/src/main/java/com/hj/blog/model/Board.java
+++ develog/src/main/java/com/hj/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.hj.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,14 @@ public class Board {
 	@ColumnDefault("0")	//홑따옴표 없음
 	private int count; // 조회수
 	
-	@ManyToOne //Many = Board, User = One,  한명의 유저는 여러개의 게시글을 쓸 수 있다.
+	@ManyToOne(fetch = FetchType.EAGER) //Many = Board, User = One,  한명의 유저는 여러개의 게시글을 쓸 수 있다.
 	@JoinColumn(name="userId")
 	private User user; // DB는 오브젝트를 저장할수 없다, FK, 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다.(난 FK가 아니에요) DB에 칼럼을 만들지 마세요 조인의 답만 필요해요
+																	  // "board" 는 필드이름. Reply테이블의 필드이름을 적으면 됨
+	//@JoinColumn 데이터베이스 1정규형 규칙(원자성)을 깨버림. 하면안됨.
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
