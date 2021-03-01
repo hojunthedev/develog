946a367f2ae6857fe31f0b85fbe02152c18c0e34
hojunthedev <ghwns5338@gmail.com>
2021-03-01 20:48:55 +0900

    select 테스트, null판정, 메세지컨버터 이해
    findById, orElseThrow, 타입 주의

--- develog/src/main/java/com/hj/blog/test/DummyControllerTest.java
+++ develog/src/main/java/com/hj/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.hj.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,35 @@ import com.hj.blog.model.RoleType;
 import com.hj.blog.model.User;
 import com.hj.blog.repository.UserRepository;
 
-@RestController
+@RestController // data를 리턴해주는 컨트롤러
 public class DummyControllerTest {
 	
 	@Autowired //UserRepository 타입으로 스프링이 관리하고있으면(메모리에 떠있으면) 같이 메모리에 띄워줘라(의존성 주입, DI)
 	private UserRepository userRepository;
+	
+	// {id} 주소로 파라메터를 전달받을 수 있음
+	// http://localhost:8000/develog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// null 리턴 방지용 Optional로 User객체를 감싸서 가져올테니 null인지 판단해서 return하세요!
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+			}
+		});
+		// 람다식
+		// User user = userRepository.findById(id).orElseThrow(() -> {
+		// 	return new IllegalArgumentException("해당 사용자가 없습니다.");
+		// })
+		// 요청 : 웹브라우저
+		// user객체 = 자바오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약 자바 오브젝트를 리턴하게 되면 MC가 Jackson 라이브러리를 호출해서
+		// User 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
+		return user;
+	}
 	
 	// http://localhost:8000/develog/dummy/join (요청)
 	// http의 body에 username, password, email 데이터를 가지고 요청
