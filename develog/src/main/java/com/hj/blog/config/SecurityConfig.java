99966fdb8056cd69218d6adc962994bddb25b638
hojunthedev <ghwns5338@gmail.com>
2021-03-16 22:37:50 +0900

    비밀번호 해쉬, 해쉬 확인, 회원가입 확인

--- develog/src/main/java/com/hj/blog/config/SecurityConfig.java
+++ develog/src/main/java/com/hj/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.hj.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는것
 // 아래 세가지는 세트라고 생각하면 편함.
@@ -13,11 +15,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {	
 	
+	@Bean // IoC가 되요!! 리턴하는 것을 스프링이 관리함
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
 			.authorizeRequests()
-				.antMatchers("/auth/**")
+				.antMatchers("/" ,"/auth/**" , "/js/**" , "/css/**" , "/image/**" )
 				.permitAll()
 				.anyRequest()
 				.authenticated()
