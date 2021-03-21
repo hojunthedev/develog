591bce657459444a2cb6e98207360f7f26aa0fc0
hojunthedev <ghwns5338@gmail.com>
2021-03-21 23:09:51 +0900

    세션유저 비교후 글 삭제처리

--- develog/src/main/java/com/hj/blog/service/BoardService.java
+++ develog/src/main/java/com/hj/blog/service/BoardService.java
@@ -1,38 +1,50 @@
 package com.hj.blog.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hj.blog.config.auth.PrincipalDetail;
 import com.hj.blog.model.Board;
 import com.hj.blog.model.User;
 import com.hj.blog.repository.BoardRepository;
 
 @Service
 public class BoardService {
-	
+
 	@Autowired
 	private BoardRepository boardRepository;
-	
+
 	@Transactional
 	public void 글쓰기(Board board, User user) {// title, content
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
 	}
-	
-	public Page<Board> 글목록(Pageable pageable){
+
+	@Transactional(readOnly = true)
+	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
 	}
-	
+
+	@Transactional(readOnly = true)
 	public Board 글상세보기(int id) {
-		return boardRepository.findById(id)
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
-				});
+		return boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
+		});
+	}
+
+	@Transactional
+	public void 글삭제하기(int id, PrincipalDetail principal) {
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 찾기 실패 : 해당 글이 존재하지 않습니다.");
+		});
+		if (board.getUser().getId() != principal.getUser().getId()) {
+			throw new IllegalStateException("글 삭제 실패 : 해당 글을 삭제할 권한이 없습니다.");
+		}
+		boardRepository.delete(board);
 	}
 
 }
--- develog/src/main/resources/static/js/board.js
+++ develog/src/main/resources/static/js/board.js
@@ -2,6 +2,9 @@ let index ={
 		init: function(){
 			$("#btn-save").on("click", () => { 
 				this.save();
+			}),
+			$("#btn-delete").on("click", () => { 
+				this.deleteById();
 			});
 		},
 
@@ -19,6 +22,21 @@ let index ={
 				dataType: "json" 
 			}).done(function(resp){
 				alert("글쓰기가 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(err){
+				alert(JSON.stringify(err));
+			});
+		},
+		
+		deleteById: function(){
+			var id = $("#id").text();
+			
+			$.ajax({
+				type: "DELETE",
+				url: "/api/board/"+id,
+				dataType: "json" 
+			}).done(function(resp){
+				alert("삭제가 완료되었습니다.");
 				location.href = "/";
 			}).fail(function(err){
 				alert(JSON.stringify(err));
